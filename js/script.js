77dd83f97f9dce6e942bd69abcaef60076dc2740
Piotr Wilczynski <tancerz@autograf.pl>
2019-10-09 11:03:35 +0200

    Add class to clicked link

--- js/script.js
+++ js/script.js
@@ -6,17 +6,19 @@ document.getElementById('test-button').addEventListener('click', function(){
 });
 */
 const titleClickHandler = function(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
 
-  /* remove class 'active' from all articles */
+  /* [DONE} remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post.active');
 
   for(let activeArticle of activeArticles){
